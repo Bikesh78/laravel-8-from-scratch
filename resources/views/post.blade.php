474456c34f679df90f30dcdc9060e86e6671f4aa
Bikesh <bikeshrai78@gmail.com>
2024-02-18 12:17:55 +0545

    Chapter 14

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,9 @@
 
 <body>
     <article>
-        <h1><?= $post->title; ?></h1>
-        <div><?= $post->body; ?></div>
+        <!-- <h1><?= $post->title; ?></h1> -->
+        <h1>{{$post->title}}</h1>
+        <!-- <div><?= $post->body; ?></div> -->
+        <div>{!! $post->body!!}</div>
     </article>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,11 +5,11 @@
 
 
 <body>
-    <?php foreach ($posts as $post) : ?>
-        <article>
-            <h1><a href="post/<?= $post->slug ?>"> <?= $post->title; ?></a></h1>
+    @foreach ($posts as $post)
+    <article>
+        <h1><a href="post/{{$post->slug}}"> {{$post->title}}</a></h1>
 
-            <div><?= $post->excerpt ?></div>
-        </article>
-    <?php endforeach; ?>
+        <div><?= $post->excerpt ?></div>
+    </article>
+    @endforeach
 </body>
